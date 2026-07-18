3c729d7f78a9e16917f3c718b0401c259ceaa2a9
aqeel <aqeelrahman69554@gmail.com>
2026-07-18 14:51:33 +0700

    feat(routes): make route for publisher

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\PublisherController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/publisher', [PublisherController::class, 'index'])->name('publihser.index');
+Route::post('/publisher', [PublisherController::class,'store'])->name('publisher.store');
+Route::put('/publisher/{id}', [PublisherController::class, 'update'])->name('publisher.update');
+Route::delete('/publisher/{id}', [PublisherController::class, 'destroy'] )->name('publisher.destroy');
