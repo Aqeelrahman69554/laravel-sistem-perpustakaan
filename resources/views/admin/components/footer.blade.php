89460a5f2b9263aca210d49305653f954345e663
aqeel <aqeelrahman69554@gmail.com>
2026-07-19 14:09:00 +0700

    feat(view): include template for admin

--- resources/views/admin/components/footer.blade.php
+++ resources/views/admin/components/footer.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+<footer class="admin-footer">
+    <div class="container-fluid px-3 px-lg-4">
+        <span>Copyright 2026 adminHMD. <br> Developed by <a target="_blank" class="fw-bold text-success"
+                href="https://github.com/HasanMahmudDev">Md. Hasan Mahmud</a> • Distributed by <a target="_blank"
+                class="fw-bold text-success" href="https://themewagon.com">ThemeWagon</a>
+        </span>
+        <span>Professional dashboard template.</span>
+    </div>
+</footer>
--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,3 +1,29 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+
+@include('admin.partials.head')
+
+<body>
+    <div class="admin-shell">
+        <div class="sidebar-backdrop" data-sidebar-close></div>
+
+        {{-- INCLUDE SIDEBAR --}}
+        @include('admin.components.sidebar')
+
+        <div class="admin-main">
+
+            {{-- INCLUDE NAVBAR --}}
+            @include('admin.components.navbar')
+
+            {{-- @include('admin.pages.dashboard') --}}
+            @yield('content')
+
+            {{-- INCLUDE FOOTER --}}
+            @include('admin.components.footer')
+        </div>
+    </div>
+    {{-- INCLUDE SCRIPT --}}
+    @include('admin.partials.script')
+</body>
+
+</html>
