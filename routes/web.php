a4b461d7310240b142dddc17ae085d1c42956ac7
aqeel <aqeelrahman69554@gmail.com>
2026-07-19 14:07:47 +0700

    test(route): make route for the time being just for test

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/main', function () {
+    return view('admin.layouts.master');
+});
+
+Route::get('/dashboard', function(){
+    return view('admin.pages.dashboard');
+})->name('admin.dashboard');
+
+Route::get('/detail-user', function () {
+    return view('admin.pages.user');
+})->name('user.user-detail');
+
 Route::get('/publisher', [PublisherController::class, 'index'])->name('publihser.index');
-Route::post('/publisher', [PublisherController::class,'store'])->name('publisher.store');
+Route::post('/publisher', [PublisherController::class, 'store'])->name('publisher.store');
 Route::put('/publisher/{id}', [PublisherController::class, 'update'])->name('publisher.update');
-Route::delete('/publisher/{id}', [PublisherController::class, 'destroy'] )->name('publisher.destroy');
+Route::delete('/publisher/{id}', [PublisherController::class, 'destroy'])->name('publisher.destroy');
